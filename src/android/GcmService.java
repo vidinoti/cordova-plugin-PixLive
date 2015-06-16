aa26cfcdef75cf41845d8a12a048eaa48a0accc2
Mathieu Monney <mathieu.monney@vidinoti.com>
2015-06-16 14:25:21 +0200

    Adding missing import in GCM Service

--- src/android/GcmService.java
+++ src/android/GcmService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
