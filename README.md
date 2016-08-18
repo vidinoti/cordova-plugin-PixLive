cf2b581b958f1cc91c76d0ab3606a1428bb9ffec
loic <loic.perruchoud@vidinoti.com>
2016-08-18 17:11:09 +0200

    improved doc

--- README.md
+++ README.md
@@ -5,19 +5,20 @@ This allows a seamless bridge for using Augmented Reality PixLive SDK into your 
 Check [http://www.pixlive.info](http://www.pixlive.info) for more information and the Ionic plugin at [http://vidinoti.github.io/angular-pixlive/](http://vidinoti.github.io/angular-pixlive/)
 
 ## Installation
-Create a PixLive Maker account, obtain a licence, download SDKs and register you app at https://www.vidinoti.com/
+Create a PixLive Maker account, obtain a licence, download SDKs and register you app at http://pixlivemaker.com
 
 
-Install the plugin by passing the VDARSDK.framework and vdarsdk-release.aar file of the PixLive SDK to the plugin installation command line:
+Install the plugin by passing the PixLive SDK VDARSDK.framework and vdarsdk-release.aar file paths and the license to the plugin installation command line:
 
 ```bash
 cordova plugin add cordova-plugin-pixlive@latest --variable LICENSE_KEY=MyLicenseKey --variable PIXLIVE_SDK_IOS_LOCATION=\"path/to/VDARSDK.framework\" --variable PIXLIVE_SDK_ANDROID_LOCATION=\"path/to/android/vdarsdk-release.aar\"
 ```
 
-## Create an augmented reality app in two minutes!
-This demo app show you the basis to use PixLive SDK in your cordova project.
 
-* Create a new Cordova project using cordova-template-PixLive. **Don't forget to register your app on PixLive Maker.**
+## Create an augmented reality app in two minutes!
+Thes best way to start developing your app is with a simple example. This demo app show you the basis to use PixLive SDK in your cordova project.
+
+* Create a new Cordova project using cordova-template-PixLive. **Don't forget to register your app on http://pixlivemaker.com.**
 
 ```bash
 cordova create myArApp com.mycompany.myArApp myArApp --template https://github.com/vidinoti/cordova-template-PixLive.git
@@ -46,4 +47,125 @@ cordova build ios
 * Your demo app is ready, you can already test it!
 ```bash
 cordova run android
+```
+
+
+## The basis to build an AR app with PixLive SDK
+
+* #### Content synchronization
+To be able to trigger AR content you need to synchronize your app with your PixLive Maker account first.
+```javascript
+//Synchronize the app with PixLive Maker http://pixlivemaker.com
+if (window.cordova && window.cordova.plugins && window.cordova.plugins.PixLive) {
+    // You can pass an array of tags to synchronize with specific tags: synchronize(['test1','test2'])
+    cordova.plugins.PixLive.synchronize([], synchSuccessListener, synchErrorListener);
+}
+```
+
+* #### Create an ARview
+The first time you open an ARview you need to create it.
+```javascript
+/**
+ * Example of arView size
+ * @return {number[]} [screenWidth,screenHeight]
+ */
+getArViewSize = function() {
+    if(window.orientation == 90 || window.orientation == -90) {
+        if(window.screen.height > window.screen.width) {
+            return [window.screen.height, window.screen.width];
+        }
+    }
+    return [window.screen.width, window.screen.height];
+};
+if (window.cordova && window.cordova.plugins && window.cordova.plugins.PixLive) {
+	var arViewSize = getArViewSize();
+	var arView = cordova.plugins.PixLive.createARView(0, 0, arViewSize[0], arViewSize[1]);
+}
+```
+
+* #### Handle the orientation changes
+```javascript
+/**
+ * onOrientationchange Event listener
+ */
+onOrientationchange = function() {
+	if(arView) {
+	 var screenSize = getSize();
+	 arView.resize(0, 0, screenSize[0], screenSize[1]);
+	}
+};
+window.addEventListener("orientationchange", onOrientationchange, false);
+```
+
+* #### Close the arView
+```javascript
+if(arView) {
+   arView.beforeLeave();
+   arView.afterLeave();
+}
+```
+
+* #### Reopen the arView
+```javascript
+if(arView) {
+    arView.beforeEnter();
+    onOrientationchange();
+    arView.afterEnter();
+}
+```
+
+* #### Handle PixLive events
+```javascript
+//event listeners for pixlive events
+var pxlEventListeners={};
+//Event handler for pixlive events
+var pxlEventHandler = function(event) {
+    if(event.type && pxlEventListeners[event.type]) {
+        for(var i = pxlEventListeners[event.type].length-1; i>=0; i--) {
+            pxlEventListeners[event.type][i](event);
+        }
+    }
+};
+/**
+ * Add a new listener for the provided event type. 
+ * @param {string} event The event to register for. 
+ * @param {function} callback The function to be called when the provided event is generated.
+ */
+addListener = function(event, callback) {
+	if(!pxlEventListeners[event]) {
+	    pxlEventListeners[event]=[];
+	}
+	pxlEventListeners[event].push(callback);
+}
+//register pxlEventHandler
+if (window.cordova && window.cordova.plugins && window.cordova.plugins.PixLive && !window.cordova.plugins.PixLive.onEventReceived) {
+   cordova.plugins.PixLive.onEventReceived = pxlEventHandler;
+}
+```
+PixLive events are: 
+	* pxlContextEnter - When enter a context (recognize image, enter beacon area)
+	* pxlContextExit - When exist a context
+	* pxlCodeRecognize - When a QR code is recognized
+	* pxlAnnotationsPresent - When content is shown
+	* pxlAnnotationsHide - When content is hidden
+	* pxlSynchronizationRequired - When a context ask to synchronize with given tags
+	* pxlSensorTriggered - When a sensor is detected
+	* pxlSensorUpdate - When a sensor value is updated
+	* pxlSensorUntriggered - When a sensor is not detected anymore
+	* pxlEventFromContent - When an event is generate from the content (PixLiveJS)
+
+For example:
+```javascript
+//enable PixLive SDK to catch the touch event when a content is displayed
+addListener("presentAnnotations",function(event){
+    arView.enableTouch();
+});
+//disable PixLive SDK to catch the touch event when a content is hidden
+addListener("hideAnnotations",function(event){
+    arView.disableTouch();
+});
+//take action when a QR code is recognized by the SDK
+addListener("codeRecognize",function(event){
+    alert("QR code recognized: " + event.code);
+});
 ```
--- www/PixLive.js
+++ www/PixLive.js
@@ -1,10 +1,19 @@
 var exec = require('cordova/exec');
 
-
 /**
- * PixLive
- * @constructor
- */
+* cordova-plugin-PixLive based class
+* <p>Use cordova.plugins.PixLive to access the static methods. The instance methods need to be called on the PixLive
+* instances returned by the createARView method, if you do not called the instance methods when it is needed problems will occur.
+* <p>Your app need to be linked with your PixLive Maker account http://pixlivemaker.com. </p>
+* To do so you need to register your app in the PixLive SDK->My Applications section 
+* and use a valid account license (PixLive SDK->My license) when you install the plugin.</p>
+* <p>Glossary:</p>
+* <ul>
+* <li> context : A context can be an image or a beacon.
+* <li> content: A content displayed when a context is trigger. 
+* </ul>
+* @class
+*/
 var PixLive = function(handle) {
 	this.options = {};
 	this.handle = handle;
@@ -13,6 +22,10 @@ var PixLive = function(handle) {
 PixLive.nextViewHandle = 1;
 
 
+/**
+* Context
+* @class
+*/
 PixLive.Context = function(prop) {
 	// Apply properties to the context object
 	var keys = Object.keys(prop);
@@ -22,41 +35,79 @@ PixLive.Context = function(prop) {
 };
 
 PixLive.Context.prototype = {
+	/**
+	* activate a context (trigger the content)
+	*/
 	activate: function() {
 		exec(null, null, "PixLive", "activateContext", [this.contextId]);
 	},
+	/**
+	* ignore a context (the context will not be activated in the future)
+	*/
 	ignore: function() {
 		exec(null, null, "PixLive", "ignoreContext", [this.contextId]);
 	}
 };
 
 PixLive.prototype = {
+	/**
+	* Need to be called before enter an arView.
+	*/
 	beforeEnter: function() {
 		exec(null, null, "PixLive", "beforeEnter", [this.handle]);
 	},
+	/**
+	* Need to be called after enter an arView.
+	*/
 	afterEnter: function() {
 		exec(null, null, "PixLive", "afterEnter", [this.handle]);
 	},
+	/**
+	* Need to be called before leaving an arView
+	*/
 	beforeLeave: function() {
 		exec(null, null, "PixLive", "beforeLeave", [this.handle]);
 	},
+	/**
+	* Need to be called after leaving an arView
+	*/
 	afterLeave: function() {
 		exec(null, null, "PixLive", "afterLeave", [this.handle]);
 	},
+	/**
+	* Destroy the Ar view
+	*/
 	destroy: function() {
 		exec(null, null, "PixLive", "destroy", [this.handle]);
 	},
+	/**
+	* Need to be called when the screen if resized ("orientationchange" window event)
+	*/
 	resize: function(originx, originy, width, height) {
 		exec(null, null, "PixLive", "resize", [this.handle, originx, originy, width, height]);
 	},
+	/**
+	* disable PixLive SDK to catch the touch event when a content is displayed
+	*/
 	disableTouch: function() {
 		exec(null, null, "PixLive", "disableTouch", []);
 	},
+	/**
+	* enable PixLive SDK to catch the touch event when a content is displayed
+	*/
 	enableTouch: function() {
 		exec(null, null, "PixLive", "enableTouch", []);
 	}
 };
 
+/**
+* Create a new AR View. Use it only once per ARView. 
+* @param {integer} originx - The x origin of the AR view in pxl
+* @param {integer} originy - The y origin of the AR view in pxl
+* @param {integer} width - The width of the AR view in pxl
+* @param {integer} height - The height of the AR view in pxl
+* @returns {PixLive} - the PixLive instance related to this AR view
+*/
 PixLive.createARView = function( originx, originy, width, height ) {
 	var handle = PixLive.nextViewHandle++;
 
@@ -66,22 +117,58 @@ PixLive.createARView = function( originx, originy, width, height ) {
 
 PixLive.onEventReceived = null;
 
+/**
+* Activate the notifactions support
+* @param {boolean} enabled - true to enable, false to disable
+* @param {string} apiKey - Google APIs project number for android app
+*/
 PixLive.setNotificationsSupport = function( enabled, apiKey ) {
 	exec(null, null, "PixLive", "setNotificationsSupport",  [ enabled ? (apiKey ? apiKey : true) : null]);
 };
 
+/**
+* Synchronize the app with the linked PixLive Maker account http://pixlivemaker.com
+* @param {string[]} tags - An array of tags to synchronize with (can be left empty) example:
+* <ul>
+* <li> [] if you do not want to use tags, all the contexts from the linked PixLive Maker account will be synchronized
+* <li> ['tag1','tag2'] to synchronize with the contexts that are tagged with tag1 and tag2
+* <li> [['tag1','tag2'], 'tag3'] to synchronize with the contexts that are tagged with (tag1 and tag2) or tag3
+* </ul>
+* Specific languages can be attributated to PixLive Maker content, to synchronize your app with a specific language
+* use the tag: 'lang_{{iso_code_of_the_language}}' for example:
+* <ul>
+* <li> ['lang_fr'] to sychronize with all the french contents
+* <li> [['tag1','lang_en'], ['tag2','lang_en']] to synronize with the english contents tagged with tag1 or tag2
+* </ul>
+* @param {callback} success - success callback
+* @param {callback} error - error callback
+*/
 PixLive.synchronize = function( tags, success, error ) {
 	exec(success, error, "PixLive", "synchronize",  [tags]);
 };
 
+/**
+* Will show the list of beacons notifications previously received
+* @param {callback} success - success callback
+* @param {callback} error - error callback
+*/
 PixLive.presentNotificationsList = function(success, error) {
 	exec(success, error, "PixLive", "presentNotificationsList",  []);
 };
 
+/**
+* Will open an url with the PixLive SDK internal browser
+* @param {string} url - The url
+*/
 PixLive.openURLInInternalBrowser = function(url) {
 	exec(success, error, "PixLive", "openURLInInternalBrowser",  [url]);
 };
 
+/**
+* Get all the contexts that have been synchronized. A context can be an image or a beacon
+* @param {callback} success(list) - success callback with the list of contexts as parameter
+* @param {callback} error - error callback
+*/
 PixLive.getContexts = function(success, error) {
 	exec(function(list) {
 		if(success !== null) {
@@ -99,13 +186,20 @@ PixLive.getContexts = function(success, error) {
 	}, error, "PixLive", "getContexts",  []);
 };
 
-// Activates the bookmark feature. A bookmark icon will be shown when a context is
-// displayed and the user has the possibility to "save" the context.
+/**
+* Activates the bookmark feature. A bookmark icon will be shown when a context is
+* displayed and the user has the possibility to "save" the context.
+* @param {boolean} enabled - true to enable, false to disable
+*/
 PixLive.setBookmarkSupport = function (enabled) {
 	exec(null, null, "PixLive", "setBookmarkSupport", [enabled]);
 };
 
-// Returns the list of contexts that have been bookmarked.
+/**
+* Returns the list of contexts that have been bookmarked.
+* @param {callback} success(list) - success callback with the list of bookmarked contexts as parameter
+* @param {callback} error - error callback
+*/
 PixLive.getBookmarks = function(success, error) {
 	exec(function(list) {
 		if(success !== null) {
@@ -120,17 +214,28 @@ PixLive.getBookmarks = function(success, error) {
 	}, error, "PixLive", "getBookmarks",  []);
 };
 
-// Adds the contextId to the list of bookmarked content.
+/**
+* Adds the contextId to the list of bookmarked content.
+* @param {string} contextId - id of the context
+*/
 PixLive.addBookmark = function(contextId) {
 	exec(null, null, "PixLive", "addBookmark", [contextId]);
 };
 
-// Removes the contextId from the list of bookmarked content.
+/**
+* Removes the contextId from the list of bookmarked content.
+* @param {string} contextId - id of the context
+*/
 PixLive.removeBookmark = function(contextId) {
 	exec(null, null, "PixLive", "removeBookmark", [contextId]);
 };
 
-/** The callback success is called with true or false depending if the context ID is bookmarked or not. */
+/**
+* The callback success is called with true or false depending if the context ID is bookmarked or not.
+* @param {string} contextId - id of the context
+* @param {callback} success - success callback
+* @param {callback} error - error callback
+*/
 PixLive.isBookmarked = function(contextId, success, error) {
 	exec(success, error, "PixLive", "isBookmarked", [contextId]);
 };
